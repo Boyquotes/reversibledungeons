c59afaa522cb339eb17d56e3a8b479f94b72451b
crize <34015092+gurencrize@users.noreply.github.com>
2023-01-09 18:00:50 +0900

    タイトル画面追加 (#11)
    ボタン押せばダンジョンはいれるよ

---
+++ Title/Title.gd
@@ -0,0 +1,10 @@
+extends Node
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+    if Input.is_anything_pressed():
+        #まだダンジョン突入までの導線を作っていないのでダンジョンに直接移動
+        get_tree().change_scene_to_file("res://Dungeon.tscn")
+    pass
+    
+
